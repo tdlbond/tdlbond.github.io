f4e0bd07463381b8e38c73f0417c0534d26548b8
tangdalu <tangdl134@163.com>
2022-04-11 17:31:00 +0800

    add router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 10020,
+    open: true
+  }
 })
